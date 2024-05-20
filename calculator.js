66b5b9272fe73d267aa7a27e6a3a1105ecf41213
Darron Dai <daidarron@gmail.com>
2024-05-20 13:10:16 -0700

    Create global display variables and operate func

--- calculator.js
+++ calculator.js
@@ -13,3 +13,11 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2;
 }
+
+let operator;
+let num1;
+let num2;
+
+function operate(operator, num1, num2) {
+  return operator(num1, num2);
+}
