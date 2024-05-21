d59c64f11550d03cd2d42bc4af07eb7324acd6f5
Darron Dai <daidarron@gmail.com>
2024-05-20 17:02:45 -0700

    Add operator listener and operator parsing helper

--- calculator.js
+++ calculator.js
@@ -18,6 +18,22 @@ function operate(operator, num1, num2) {
   return operator(num1, num2);
 }
 
+function parseOperator(operatorString) {
+  switch (operatorString) {
+    case "add":
+      return add;
+    case "subtract":
+      return subtract;
+    case "multiply":
+      return multiply;
+    case "divide":
+      return divide;
+    default:
+      console.log("add by default");
+      return add;
+  }
+}
+
 // the operator and enteredNumber will be strings, but the saved result is a number
 // savedResult starts as undefined but gets assigned when pressing an op btn for the first time
 let operator;
@@ -28,6 +44,32 @@ const numBtnContainer = document.querySelector(".number-btn-container");
 
 numBtnContainer.addEventListener("click", (event) => {
   if (event.target.tagName === "BUTTON") {
-    console.log(+event.target.value);
+    // string concatenation
+    enteredNumber += event.target.value;
+    console.log(enteredNumber);
   }
 });
+
+const operatorBtnContainer = document.querySelector(".operator-btn-container");
+operatorBtnContainer.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    // if no saved result, we save the entered number to the saved result
+    if (!savedResult) {
+      savedResult = +enteredNumber;
+    }
+    // otherwise, calc using previously saved operator(savedResult, parsed enteredNumber)
+    else {
+      console.log(operator);
+      savedResult = operate(operator, savedResult, +enteredNumber);
+    }
+    // parse and save new operator, and flush entered number
+    enteredNumber = "";
+    const operatorString = event.target.value;
+    operator = parseOperator(operatorString);
+    console.log(savedResult);
+  }
+});
+
+// show the saved result after operating, and use it as the first number for any operation
+// when the entered number is empty, show the saved result in the display instead
+// only show the entered number until either an operation is pressed or the equal
